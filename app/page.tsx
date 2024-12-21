24530e8c76d27868ce734e623250466c3f55deed
soumava <soumavadas06@gmail.com>
2024-12-21 20:18:53 +0530

    Searchbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Searchbar from '@/components/Searchbar';
 import Image from 'next/image'
 import React from 'react'
 
@@ -27,7 +28,7 @@ const Home = () => {
               convert, engage, and retain more.
             </p>
 
-            Searchbar
+            <Searchbar />
           </div>
 
           HeroCarousel
